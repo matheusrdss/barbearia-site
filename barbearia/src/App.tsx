1de6a74b08ab715364dd61c3b7ae0b9bb60555f6
Matheus <matheusrdasilvasantos@gmail.com>
2022-06-28 20:21:38 -0300

    a

--- barbearia/src/App.tsx
+++ barbearia/src/App.tsx
@@ -7,6 +7,14 @@ function App() {
     <>
       <div className="App">
         <Navbar />
+        <div className="infos">
+          <div className="info-left">
+            <h1 className="titulo">Você não pode confiar em qualquer um com seu cabelo.</h1>
+            <p className="sobre">A eficiencia que você procura somente nós podemos proporcionar</p>
+            <button className="contato">Entrar em contato</button>
+          </div>
+          <div className="info-right"></div>
+        </div>
       </div>
     </>
   );
